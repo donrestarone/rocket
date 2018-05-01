65ced779b0b8ef83a5d6636ab88ae15826cfe83c
donrestarone <shashikejayatunge@gmail.com>
2018-05-01 13:40:31 -0400

    start assignment, 2 tests passing

--- rocket_test.rb
+++ rocket_test.rb
@@ -2,5 +2,24 @@ require "minitest/autorun"
 require_relative "rocket"
 
 class RocketTest < Minitest::Test
-  # Write your tests here!
+  def setup
+  	@rocket = Rocket.new(name: "Powerful North Korean Rocket")
+  	@random_rocket = Rocket.new
+  end 
+
+  def test_name_reader
+  	expected_value = "Powerful North Korean Rocket"
+  	actual_value = @rocket.name 
+  	assert_equal(expected_value, actual_value)
+  end 
+
+  def test_name_writer
+  	expected_value = "Broken North Korean Rocket"
+  	actual_value = @rocket.name=("Broken North Korean Rocket")
+  	assert_equal(expected_value, actual_value)
+  end
+
+  def test_color_reader
+  	
+  end 
 end
